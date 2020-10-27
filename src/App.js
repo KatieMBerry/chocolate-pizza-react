7c837ca2746f787e215e3c6290e0d5f1a9d9df40
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-26 17:52:09 -0700

    added ingredient list and items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header.js';
 import MainContent from './Main-Content.js';
 import './App.css';
+import IngredientsList from './IngredientsList.js';
 
 export default class App extends React.Component {
   render() {
@@ -9,7 +10,7 @@ export default class App extends React.Component {
       <div className="main">
         <Header />
         <MainContent />
-
+        <IngredientsList />
       </div >
     );
   }
--- src/Main-Content.js
+++ src/Main-Content.js
@@ -14,7 +14,6 @@ export default class MainContent extends Component {
                 <p>For the fig-swirl: Melt butter over medium heat in a saucepan. Add brown siugar and stir to dissolve. Halve all of the figs and toss in the saucepan with water and lemon juice. Cook over medium heat, stirring frequently, until you have a chunky-jammy mixture. Add salt with one or two stirs, set aside and let cool completely.</p>
                 <p>Ice cream: In a small pot over medium heat, combine milk, add granulated sugar until sugar is completely dissolved and the milk is just barely luke warm. Whisk in the egg yolks. Set mixture in the fridge and wait until the fig mixture cooled.</p>
                 <p>Using and ice cream machine, pour liquids into the frozen basin and process according to manufacturer instructions, ie, let spin and thicken for 20 minutes before adding marscapone, fig jam mixture, and the nuts. Continue to process for +/- 10 minutes. Pour semi frozen mixture into a pyrex dish or glass tupperware. Freeze for at least 2 hours before serving.</p>
-
             </div>
         )
     }
